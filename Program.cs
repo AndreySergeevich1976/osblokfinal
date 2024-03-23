42be0cac3bfd863697a461c2031b1c4bfc57378d
Andrey Krasnov <tiralmaz@mail.ru>
2024-03-23 16:31:45 +0300

    создал и обозначил массивы строчные

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string[] workArray = FillArray();
+string[] resultArray = GenerateNewArray(workArray);
+string firstArray = PrintArray(workArray);
+string secondArray = PrintArray(resultArray);
+Console.WriteLine(firstArray + " -> " + secondArray);
+
